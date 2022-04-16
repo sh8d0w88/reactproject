64e75cb4d3de8043005c455058e63581b0fe1040
sh8d0w88 <103542227+sh8d0w88@users.noreply.github.com>
2022-04-15 22:02:21 -0400

    Fixed missing bootstrap
    Forgot to install bootstrap, only had react-bootstrap.
    
    Imported bootstrap css in index.js
    Fixed the collapse in Nav.js
    Working as intended.

--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -4,15 +4,16 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 function Navigation() {
     return (
         <>
-            <Navbar collapseOnSelect fixed='top' expand='sm' bg='dark' variant='dark'>
+            <Navbar collapseOnSelect sticky='top' expand='sm' bg='dark' variant='dark'>
                 <Container>
                     <Navbar.Toggle aria-controls='responsive=nabar-nav' />
-                    <Navbar.Collapse id='responsive-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
                     <Nav>
                         <Nav.Link href="/" className="item">Projects</Nav.Link>
                         <Nav.Link href="/articles" className="item">Articles</Nav.Link>
                         <Nav.Link href="/about" className="item">About</Nav.Link>
                     </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
